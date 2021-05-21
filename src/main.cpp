10210ddbcfa19f34f4fbf5bb31449b431df2f135
Lucas Garofalo <lucasg.18@hotmail.fr>
2021-05-21 08:54:15 +0200

    Added try catch + eye candies in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include "CGraphReader.h"
 #include "CGraphException.h"
+#include <iostream>
+
+using namespace std;
 
 /**
  * Program entry point, run tests with the provided files as arguments
@@ -9,17 +12,26 @@
  * @return The program exit code
  */
 int main(int argc, char** argv){
-    if (argc == 2) {
-        auto reader = new CGraphReader(argv[1]);
-        CGraph* graph = reader->GRRRead();
-        graph->GRAPrint();
-        CGraph* invertedGraph = graph->GRAInvert();
-        invertedGraph->GRAPrint();
-        delete reader;
-        delete graph;
-        delete invertedGraph;
-    } else {
-        throw CGraphException(GRAPH_EXCEPTION_INVALID_PROGRAM_PARAMETERS, strMultiCat(3,
-            "Example program need 1 parameter, given ", itoa(argc)));
+    try {
+        if (argc == 2) {
+            printf("Graph %s:", argv[1]);
+            auto reader = new CGraphReader(argv[1]);
+            CGraph* graph = reader->GRRRead();
+            graph->GRAPrint();
+            printf("\n");
+            printf("Graph %s inverted:", argv[1]);
+            CGraph* invertedGraph = graph->GRAInvert();
+            invertedGraph->GRAPrint();
+            delete reader;
+            delete graph;
+            delete invertedGraph;
+        }
+        else {
+            throw CGraphException(GRAPH_EXCEPTION_INVALID_PROGRAM_PARAMETERS, strMultiCat(3,
+                "Example program need 1 parameter, given ", itoa(argc)));
+        }
+    }
+    catch (CGraphException e) {
+        cerr << e.what();
     }
 }
